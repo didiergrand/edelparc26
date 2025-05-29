785080f5f7bf8ecfc338e82074a68dab4a7db3b1
Didier Grand <didier.grand@gmail.com>
2025-05-29 21:45:26 +0200

    Add title to full with page

--- js/page-fullwidth.php
+++ js/page-fullwidth.php
@@ -6,6 +6,7 @@
 get_header(); ?>
 
 <main id="primary" class="site-main container">
+<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
     <?php
     while ( have_posts() ) :
         the_post();
