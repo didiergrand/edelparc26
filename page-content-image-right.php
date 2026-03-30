147a3196d0b7b0d0d34fea929a9f1ad2e382b561
Didier Grand <didier.grand@gmail.com>
2026-03-30 15:48:39 +0200

    Adjust shadow and stars

--- page-content-image-right.php
+++ page-content-image-right.php
@@ -35,13 +35,20 @@ get_header();
 								?>
 							</div>
 							<div class="thumbnail thumbnail--decorated">
-								<span class="thumbnail-decor thumbnail-decor--panel" aria-hidden="true"></span>
 								<span class="thumbnail-decor thumbnail-decor--star thumbnail-decor--star-large" aria-hidden="true"></span>
 								<span class="thumbnail-decor thumbnail-decor--star thumbnail-decor--star-medium" aria-hidden="true"></span>
 								<span class="thumbnail-decor thumbnail-decor--star thumbnail-decor--star-small" aria-hidden="true"></span>
 								<?php
 								if ( has_post_thumbnail() ) {
+									ob_start();
 									edelparc26_post_thumbnail();
+									$thumbnail_html = ob_get_clean();
+
+									echo str_replace(
+										'<div class="post-thumbnail">',
+										'<div class="post-thumbnail"><span class="thumbnail-decor thumbnail-decor--panel" aria-hidden="true"></span>',
+										$thumbnail_html
+									);
 								}
 								?>
 							</div>
