c9de97b41dbe2b92d06776226c905c2e0d55d86f
Didier Grand <didier.grand@gmail.com>
2026-05-18 09:22:08 +0200

    Photos category layout update

--- template-parts/content-photos.php
+++ template-parts/content-photos.php
@@ -7,6 +7,13 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+	<?php if ( has_post_thumbnail() ) : ?>
+	<div class="home_actus-img">
+		<a href="<?php the_permalink(); ?>" tabindex="-1" aria-hidden="true">
+			<?php the_post_thumbnail( 'squared-size' ); ?>
+		</a>
+	</div>
+	<?php endif; ?>
 
 	<div class="home_actus-content">
 		<a href="<?php the_permalink(); ?>">
